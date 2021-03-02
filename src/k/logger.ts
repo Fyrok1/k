2451935a7b73a3e26dfb62c443273d57185dc389
Fyrok1 <tahsincesur1@gmail.com>
2021-03-02 20:08:48 +0300

    logger model replaced

--- src/k/logger.ts
+++ src/k/logger.ts
@@ -1,48 +1,6 @@
 import winston, { format } from "winston"
 import Transport from "winston-transport"
-import { sequelize } from "./sequelize";
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
-
-@Table({
-  modelName: 'Log',
-  tableName: 'logs',
-  updatedAt: false
-})
-class Log extends Model {
-  @Column({
-    type: DataType.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  })
-  id: Number;
-
-  @Column({
-    type: DataType.STRING
-  })
-  level: String;
-
-  @Column({
-    type: DataType.TEXT({ length: 'medium' }),
-    set: function (value) {
-      try {
-        const val = JSON.stringify(value)
-        this.setDataValue('message', val);
-      } catch (error) {
-        this.setDataValue('message', value);
-      }
-      this.setDataValue('message', JSON.stringify(value));
-    },
-    get: function () {
-      return JSON.parse(this.getDataValue('meta'));
-    }
-  })
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  message: any
-}
-
-if (process.env.DB == "1") {
-  sequelize.addModels([Log])
-}
+import { Log } from "./models/log.model";
 
 class SequelizeTransport extends Transport {
   constructor(opts) {
---
+++ src/k/models/log.model.ts
@@ -0,0 +1,39 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+
+@Table({
+  modelName: 'Log',
+  tableName: 'logs',
+  updatedAt: false
+})
+export class Log extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  })
+  id: Number;
+
+  @Column({
+    type: DataType.STRING
+  })
+  level: String;
+
+  @Column({
+    type: DataType.TEXT({ length: 'medium' }),
+    set: function (value) {
+      try {
+        const val = JSON.stringify(value)
+        this.setDataValue('message', val);
+      } catch (error) {
+        this.setDataValue('message', value);
+      }
+      this.setDataValue('message', JSON.stringify(value));
+    },
+    get: function () {
+      return JSON.parse(this.getDataValue('meta'));
+    }
+  })
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  message: any
+}
